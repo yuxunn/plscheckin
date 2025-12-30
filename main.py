035f8b69f62d5db16c82b7e6a1d6091b82916563
yuxunn <teeyuxun@gmail.com>
2025-12-30 23:03:58 +0800

    fix: allow all vercel regex

--- main.py
+++ main.py
@@ -65,6 +65,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_origin_regex="https://.*\.vercel\.app"
 )
 
 class BookingInput(BaseModel):
